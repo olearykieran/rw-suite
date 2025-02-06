e466133c6209790be748599f2d846f3ce02d4947
olearykieran <koleary515@gmail.com>
2025-02-06 17:07:48 -0500

    fixed about

--- src/app/public/about/page.tsx
+++ src/app/public/about/page.tsx
@@ -8,9 +8,11 @@ import { Card } from "@/components/ui/Card";
 export default function AboutPage() {
   return (
     <PageContainer>
+      <h1 className="text-2xl font-bold flex flex-col items-center text-center mt-16 space-y-4">
+        About
+      </h1>
       {/* Center the content inside a max-width container */}
-      <Card className="flex flex-col text-black items-center text-center mt-16 space-y-4">
-        <h1 className="text-2xl text-black font-bold">About</h1>
+      <Card className="flex flex-col items-center text-center mt-16 space-y-4">
         <p className="font-medium">
           RW Suite is a modern construction management software
         </p>
